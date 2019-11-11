ce05e17fecb91d656fc366d750d2bd3efb7349f9
James Phipps <james.phipps@gmail.com>
2019-11-11 17:44:15 -0500

    Initial Commit

---
+++ Fall2019/Fall2019/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Fall2019
+//
+//  Created by James Phipps on 11/11/19.
+//  Copyright © 2019 James Phipps. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
